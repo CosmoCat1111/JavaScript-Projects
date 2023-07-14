5831dab97362d76a85ac91cd3b3f2912feb04791
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2023-07-14 11:56:55 -0700

    uploading new JavaScript folder
    uploading current javascript projects

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+
+
+var Leah = "The Absolute Queen", GithubLeah = "I go by CosmoCat1111", Boss = "Leah, and you know it!", Kids = "Rugrats in my house";
+
+document.write(Leah);  /*this is a text command displaying a value called by the above set values*/
+
+document.write(5+5); /*this is calling a function to produce a value*/
