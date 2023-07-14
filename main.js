b5a594961469feab064d7d2fc02bdf8d723d1afc
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2023-07-14 12:18:15 -0700

    updating
    adding missing code wasn't aware to keep previously

---
+++ main.js
@@ -0,0 +1,12 @@
+
+window.alert("Ahoy Matey!") /*Window alert box*/
+
+document.write("Ahoy! Land Ho!")  /*document text methos*/
+
+
+/*Creating multiple variables*/
+var Leah = "The Absolute Queen", GithubLeah = "I go by CosmoCat1111", Boss = "Leah, and you know it!", Kids = "Rugrats in my house";
+
+document.write(Leah);  /*this is a text command displaying a value called by the above set values*/
+
+document.write(5+5); /*this is an expression which is calling a function to produce a value*/
